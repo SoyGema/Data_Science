c80b47b0176067fd15174a65e08e51ccfc5adcab
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-03-27 21:01:55 +0200

    Update list_non_unique_elements.py

--- DataScience/Checkio/list_non_unique_elements.py
+++ DataScience/Checkio/list_non_unique_elements.py
@@ -1,7 +1,6 @@
 
 
 #Your optional code here
-
 #You can import some modules or create additional functions
 
 ​
@@ -9,50 +8,26 @@
 ​
 
 def checkio(data):
-
     data2 = []
-
-    
-
     for element in data:
-
         if data.count(element) > 1:
-
             data2.append(element)
-
-
-
-​
-
-
     return data2
 
-​
 
 #Some hints
-
 #You can use list.count(element) method for counting.
-
 #Create new list with non-unique elements
-
 #Loop over original list
 
-​
-
-​
 
 if __name__ == "__main__":
 
     #These "asserts" using only for self-checking and not necessary for auto-testing
-
     assert isinstance(checkio([1]), list), "The result must be a list"
-
     assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
-
     assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
-
     assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
-
     assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
 
 
