7a4dd25bafbde5367e84f93563081f47f9064180
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-03-30 20:45:17 +0200

    Create IndexPower.py

---
+++ DataScience/Checkio/IndexPower.py
@@ -0,0 +1,18 @@
+def index_power(array, n):
+    if n > len(array)-1:
+        return -1
+    if n < 0:
+        return -1
+    
+    potencia = 1
+    for i in range(0,n):
+        potencia = potencia*array[n]
+    return potencia
+  
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert index_power([1, 2, 3, 4], 2) == 9, "Square"
+    assert index_power([1, 3, 10, 100], 3) == 1000000, "Cube"
+    assert index_power([0, 1], 0) == 1, "Zero power"
+    assert index_power([1, 2], 3) == -1, "IndexError"
