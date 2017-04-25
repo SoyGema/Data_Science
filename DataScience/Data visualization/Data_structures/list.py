43431aa34197f801d96ae2ee20366def64d4f176
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-04-25 18:18:05 +0200

    knowing more about lists

--- DataScience/Data visualization/Data_structures/list.py
+++ DataScience/Data visualization/Data_structures/list.py
@@ -28,3 +28,50 @@ value = [2, 3, 4]
 
 dictionary = dict(zip(keys, values))
  
+#Difference between append() and extend()
+#append() --- adds an object at the end of the list or even another list
+#extend() ---
+
+fruits = ['banana', 'apple', 'watermelon']
+#add elements to the list
+fruits.append('strawerry')
+#add list of elements to the list
+fruits.append(['pear','melon'])
+#even more clean
+fruits2 = ['pear','melon']
+fruits.append(fruits2)
+
+
+#extend is used for append the elements one by one of a given list rather
+#the list itself
+fruits.extend(fruits2)
+
+
+#method index()
+#it searches for an element in a list
+fruits.index('apple')
+
+#Operators in lists
+# + is used as an extend
+fruits += ['grapefruit']
+
+#Slicing: access to a part of the list
+#by default the first index is 0
+fruits[:] #returns all list
+fruits[2:] #from second element going beyond
+
+
+#List compresenhesion
+#straightfoward writting for loops in list data structures
+pair_numbers = []
+for i in range(10):
+    if i % 2 ==0:
+        pair_numbers.append(i)
+
+pair_numbers = [i for i in range(10) if i % 2==0]
+
+#how to copy a list
+import copy
+fruits_copy = copy.copy(fruits)
+#when having nested objects, meaning a copy of a copy
+fruits_cpoy = copy.deepcopy(fruits)
