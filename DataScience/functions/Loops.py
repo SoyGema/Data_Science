e60c4d5bf3eb5163a2a7df21454bdafd5e6c8074
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-06-19 20:33:05 +0200

    palindrome function

--- DataScience/functions/Loops.py
+++ DataScience/functions/Loops.py
@@ -24,3 +24,15 @@ def fibonacci(number):
     for value in range(2,number + 1) :
         fibo.append( fibo[value-1]+ fibo[value -2])
     return fibo
+
+
+
+
+#-------------------------------#
+def palindrome(text):
+    text_inverse = []
+    for character in range(len(text) - 1, -1, -1):
+        text_inverse.append(text[character])
+    print( ''.join(text_inverse))
+    print(text)
+    return text == ''.join(text_inverse)
