aaed260242976a3352a6f6c1089cf253cdc06da8
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-06-27 20:41:11 +0200

    more dataframe operations

--- DataScience/Pandas/Main_operators.py
+++ DataScience/Pandas/Main_operators.py
@@ -14,6 +14,25 @@ df['time_column']=df['time_column'].apply(lambda x: datetime.strptime(x,'%b-%Y')
 
 status_data = df.groupby(['column_1'])['column_2'].count()
 
-#Transform data into float
+#Transform data into float // int
 
-df['column_1']=df['column_2'].astype(float)
+df['column_1']=df['column_1'].astype(float)
+df['column_1']=df['column_1'].astype(int)
+
+#Sum the elements of a column
+
+total_column_sum = df['column_1'].sum()
+
+#Mean of the elements of a column
+
+total_column_mean = df['column_1'].mean()
+
+#Count of unique elements in a column
+
+df['column_1'].nunique()
+
+#Select certain rows from a column in a dataset 
+# values are inside the column, are values of the row
+
+row_value_selected = ['value_1', 'value_2', 'value_3']
+dataset_columns = df.loc[df['column_1'].isin(row_value_selected)]
