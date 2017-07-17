451d5f673afab843d733ec6e24a6b8d93f33b3f5
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-07-17 16:52:20 +0200

    parsing data

--- DataScience/Json/parsing.py
+++ DataScience/Json/parsing.py
@@ -6,5 +6,30 @@ import ijson
 #Read file 
 filename = "name.json"
 with open(filename, 'r') as f:
-    objects = ijson.items(f, 'meta.view.columns.item')
+    objects = ijson.items(f, 'path')
     columns = list(objects)
+print(columns[0])
+
+#Get the column names
+column_names = [col["fieldName"] for col in columns]
+column_names
+
+
+good_columns = [
+    "column_1", 
+    "column_2", 
+    "column_3", 
+]
+
+#Make an empty list and fill it with the data coming from column_1, column_2, column_3
+data = []
+with open(filename, 'r') as f:
+    objects = ijson.items(f, 'data.item')
+    for row in objects:
+        selected_row = []
+        for item in good_columns:
+            selected_row.append(row[column_names.index(item)])
+        data.append(selected_row)
+
+#See how it is going on
+data[0]
