0423965616597b7f8a35c2ca2e172492ad50b438
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-10-06 13:26:54 +0200

    Create a scattermatrix ( variable relationship)

---
+++ DataVisualization/ScatterMatrix.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('data.csv')
+plt.figure()
+
+scattermatrix = pd.scatter_matrix(dataset, alpha=0.05, figsize=(10,10), diagonal='hist)
+plt.show()
