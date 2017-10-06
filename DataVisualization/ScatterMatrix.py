fbd0d731c18ba5a6028fb40015e9f7e4f2a999d7
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-10-06 13:37:28 +0200

    Update ScatterMatrix.py

--- DataVisualization/ScatterMatrix.py
+++ DataVisualization/ScatterMatrix.py
@@ -4,5 +4,12 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv('data.csv')
 plt.figure()
 
-scattermatrix = pd.scatter_matrix(dataset, alpha=0.05, figsize=(10,10), diagonal='hist)
+scattermatrix = pd.scatter_matrix(dataset, alpha=0.05, figsize=(10,10), diagonal='hist')
 plt.show()
+
+
+# In seaborn
+import seaborn as sns 
+sns.set(style="ticks")
+
+sns.pairplot(dataset, hue="data")
