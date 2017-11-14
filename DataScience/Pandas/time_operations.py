f27021ee61494f00af9dec7d4265c6753ee85876
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-11-14 17:13:48 +0100

    Update time_operations.py

--- DataScience/Pandas/time_operations.py
+++ DataScience/Pandas/time_operations.py
@@ -8,3 +8,8 @@ data['@timestamp'].head()
 
 # Apply lambda function to datetime to extract only day-month 
 data['@timestamp'].apply(lambda x: x[5:10])
+column_date.apply(lambda x: x[5:10])
+
+#Sort the dataframe by date
+data.index = data['timestamp']
+data.sort_index()
