de08563af68d45b3995b78591b271449e85d0048
Gema Parreño <gema.parreno.piqueras@gmail.com>
2019-12-03 14:48:43 -0800

    Create queue.py

---
+++ DataScience/Data visualization/Data_structures/queue.py
@@ -0,0 +1,14 @@
+#Create queue . Head is on the left, tail is on the right
+queue = Queue()
+
+#Size of the Queue
+queue.size()
+
+#Adding elements to a queue
+queue.enqueue('element_to_add')
+
+#Remove elements from the queue (Removes the first element)
+queue.dequeue()
+
+#See how the queue looks
+queue.qlist
